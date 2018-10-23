223b076f090b44c020178b6e830bb91aff2fdf22
varad1 <43951001+varad1@users.noreply.github.com>
2018-10-24 00:26:13 +0530

    Add files via upload

---
+++ name and marks.py
@@ -0,0 +1,6 @@
+a=input("plese enter your name")
+b=input("please enter your roll number")
+c=input("please enter your gr number")
+d=int(input("please enter your maths marks")
+      f=int(input("please enter your science marks")
+x=[name:a,roll number:b,gr no.:c,maths:d,science:f)
